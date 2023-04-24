7ebe4c51dee4211749caf900bf9ba1f697753bb1
ataidechilaule <ataide1chilaule@gmail.com>
2023-04-24 05:02:25 +0200

    Binding

---
+++ app/src/main/java/com/ataide/exemplo/MainActivity.java
@@ -0,0 +1,42 @@
+package com.ataide.exemplo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.ataide.exemplo.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding mainBinding;
+
+    static final String USERNAMECONSTANTE = "lecc31";
+    static final String PASSWORDCONSTANTE = "5432A";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mainBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(mainBinding.getRoot());
+
+        mainBinding.entrarBtn.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                        String username = mainBinding.userED.getText().toString();
+                        String password = mainBinding.passED.getText().toString();
+
+                        if ( username.equals(USERNAMECONSTANTE) && password.equals(PASSWORDCONSTANTE) ){
+                            Toast.makeText(MainActivity.this, "worked", Toast.LENGTH_SHORT).show();
+                        }else {
+                            Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }
+        );
+
+    }
+}
